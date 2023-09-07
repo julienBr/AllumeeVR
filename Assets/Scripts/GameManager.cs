ea3904749db43201e7d952637816a51dc7102b84
julienBr <julien.bringard@outlook.fr>
2023-09-07 16:44:45 +0200

    Update GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,50 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private GameObject _fader;
+    [SerializeField] private GameObject _loadingScreen;
+    [SerializeField] private Slider _slider;
+    private string _currentScene;
+    private bool _oneTime;
     
+    private void Awake()
+    {
+        _currentScene = gameObject.scene.name;
+    }
+    
+    private void Update()
+    {
+        if(_currentScene == "Start" && Input.anyKeyDown) StartCoroutine(ThrowGame());
+    }
+       
+    private IEnumerator ThrowGame()
+    {
+        if(!_oneTime) _source.Play();
+        _oneTime = true;
+        yield return new WaitForSeconds(1f);
+        _fader.GetComponent<Animator>().SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadSceneAsync("HUB");
+    }
+
+    public void LoadLevel(string levelToLoad)
+    {
+        
+    }
+
+    private IEnumerator LoadAsync(string levelToLoad)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / .9f);
+            _slider.value = progressValue;
+            yield return null;
+        }
+    }
 }
